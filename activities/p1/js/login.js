708850bf910f8bc1dbfac4c6716ff5c87f92f1a7
Renek <jaes.ang.up@phinmaed.com>
2023-12-26 13:58:42 +0800

    login javascript function fixed. Account has been made and can be used to login.

--- activities/p1/js/login.js
+++ activities/p1/js/login.js
@@ -4,26 +4,43 @@ var password = document.forms['form']['password'];
 var username_error = document.getElementById("username_error");
 var pass_error = document.getElementById("pass_error");
 
-username.addEventListener('textInput', emailVerify);
+username.addEventListener('textInput', usernameVerify);
 password.addEventListener('textInput', passVerify);
 
-function validated() {
-    if(username.value.length < 9) {
+var totoongTao = {
+    username: "MemberNgPHub",
+    password: "sekretolangpo"
+};
+
+var json = JSON.stringify(totoongTao);
+localStorage.setItem(totoongTao, json);
+
+function validated(e) {
+    var user = localStorage.getItem(totoongTao);
+    var data = JSON.parse(user);
+    console.log(data);
+
+    if(username.value != data.username) {
         username.value.border = "1px solid red";
         username_error.style.display = "block";
         username.focus();
         return false;
     }
 
-    if(password.value.length < 8) {
+    if(password.value != data.password) {
         password.value.border = "1px solid red";
         pass_error.style.display = "block";
         password.focus();
         return false;
     }
+
+    if(username == data.username && password == data.password) {
+        window.location.href = "dashboard.html";
+        console.log("Logged in");
+    }
 }
 
-function emailVerify() {
+function usernameVerify() {
     if(username.value.length >= 9) {
         username.value.border = "1px solid silver";
         username_error.style.display = "none";

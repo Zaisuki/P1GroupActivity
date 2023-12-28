00bf3f8cdc415bc99f1085d5610fa270aae93110
Yurei <glba.alicoben.up@phinmaed.com>
2023-12-28 18:09:02 +0800

    fonts and display
    Co-authored-by: Renek <Arthritisboy@users.noreply.github.com>
    Co-authored-by: leyschz <LeanneDelaCruz@users.noreply.github.com>

--- activities/p1/js/verify.js
+++ activities/p1/js/verify.js
@@ -1,11 +1,11 @@
 var loginBtn = document.getElementById("login-btn")
 
-loginBtn.text = localStorage.getItem("login") === "true" ? "LOGOUT" : "LOGIN"
+loginBtn.text = localStorage.getItem("login") === "true" ? "Logout" : "Login"
 console.log(localStorage.getItem("login") === "true")
 // Log Out
 loginBtn.addEventListener("click", () => {
     var loginVerify = localStorage.getItem("login");
-    if (loginBtn.text === "LOGOUT" && loginVerify === "true") {
+    if (loginBtn.text === "Logout" && loginVerify === "true") {
         window.location.href = "login.html";
         localStorage.removeItem("login")
     }
